fbe8a36ffdf26d54cd12910290bcf5e3858e7094
Victor <victor.nicacio@usp.br>
2018-06-01 22:29:32 -0300

    Adição dos documentos base para o trabalho

---
+++ Integral.java
@@ -0,0 +1,21 @@
+/*******************************************************************/
+/** ACH2001 - Introducao a Programacao                            **/
+/** EACH-USP - Primeiro Semestre de 2015                          **/
+/**                                                               **/
+/** Segundo Exercicio-Programa                                    **/
+/**                                                               **/
+/** <Nome>  <nusp>                                                **/
+/**                                                               **/
+/*******************************************************************/
+
+class Integral {
+	/**
+		Retorna a área sob a curva definida pela função f, entre os pontos
+		a e b, pela regra dos trapézios, aproximando a curva por n trapézios.
+		
+		O método retorna -1 caso n não seja maior ou igual a 1.
+	*/
+	static double resolve(Funcao f, double a, double b, int n) {
+		// coloque seu código aqui
+	}
+}
