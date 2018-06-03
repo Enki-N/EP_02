b011dc9db6b642b7a3cf63770e56e9827c36723e
Victor <victor.nicacio@usp.br>
2018-06-03 16:15:58 -0300

    Versão_1.0 - Integral.java

--- Integral.java
+++ Integral.java
@@ -16,15 +16,19 @@ class Integral {
 		O método retorna -1 caso n não seja maior ou igual a 1.
 	*/
 	static double resolve(Funcao f, double a, double b, int n) {
-		double h = b - a;
-		double area = 0;
-		double somatorio = 0;
+		//Metodo para resolver a area da função a partir da Regra dos Trapézios.
 
-		for(int i = 1; i <= n; i++){
-			somatorio += f.valor(i);
+		double h = (b - a) / n; // Determina a altura dos trapézios.
+		double area = 0; // Variavel para armazenar o valor aproximando da área.
+		double sum = 0; // Variavel que armazena o valor do somatório.
+		double x = a + h; // Inicia os pontos que irão ser contabilizados no somatório.
+
+		for(int i = 1; i < n; i++){ // Laço para calcular o valor do somatório.
+			sum += f.valor(x);
+			x += h;
 		}
 
-		area = (h / 2) * f.valor(a) + f.valor(b) + 2 * (somatorio);
+		area = (h / 2) * (f.valor(a) + f.valor(b) + (2 * (sum))); // Aplicação da fórmula da Regra dos Trapezios.
 
 		return (area);
 	}
